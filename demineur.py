d78fc8af111ab987f7ccdc1629ebc2732f7f703e
Moonstroke <Moonstroke@users.noreply.github.com>
2016-09-28 18:10:24 +0200

    Update demineur.py

--- demineur.py
+++ demineur.py
@@ -3,69 +3,67 @@
 
 from random import randrange
 
+def get(prompt, allow_empty=False):
+    try:
+        r = input(prompt)
+    except EOFError:
+        print('')
+        return get(prompt)
+    except KeyboardInterrupt:
+        print('')
+        exit()
+else: return r if r != '' or allow_empty else get(prompt)
+
 def en_string(list):
     s = ''
     for k in list:
         s += k
     return s
 
-def get(prompt):
-    try:
-        r = input(prompt)
-    except EOFError:
-        print('')
-        get(prompt)
-    except KeyboardInterrupt:
-        print('')
-        exit()
-    else:
-        return r if r != '' else get(prompt)
-
-
-class Grille(object):
-    '''Instancie une grille de dimensions `hauteur` par `largeur`
+class Grid(object):
+    '''Instantiates a grid of dimensions `h` by `w`
 '''
     
-    def placer_mines(self, n_mines):
+    def place_mines(self, n_mines):
         for _ in range(n_mines):
             i = randrange(len(self.grille))
-            self.grille[i] = '@'
+            self.grid[i] = '@'
     
-    def en_grille(self, hauteur, largeur):
+    def to_grid(self, hauteur, largeur):
         self.grille = [self.grille[i:i + largeur] for i in range(0, len(self.grille), largeur)]
-        if len(self.grille) != hauteur: raise ValueError('Incorrect hauteur')
+        if len(self.grille) != hauteur: raise ValueError('Incorrect height')
     
-    def __init__(self, hauteur, largeur, n_mines):
-        self.grille = ['.'] * hauteur * largeur
-        self.placer_mines(n_mines)
-        self.en_grille(hauteur, largeur)
-        self.fini = False
-        self.victoire = False
+    def __init__(self, h, w, n_mines):
+        self.grille = ['.'] * h * w
+        self.place_mines(n_mines)
+        self.to_grid(hauteur, largeur)
+        self.over = False
+        self.vict = False
     
     def __repr__(self):
         r = '+' + '-' * len(self.grille[0]) + '+\n'
         for l in self.grille:
-            r += '|' + en_string(l) + '|\n'
+            r += '|' + to_string(l) + '|\n'
         r += '+' + '-' * len(self.grille[-1]) + '+\n'
         return r
     
-    def verifier_case(self, x, y):
-        try: return x >= 0 and y >= 0 and self.grille[y][x] in '@P!'
+    def check_case(self, x, y):
+        try: return x >= 0 and y >= 0 and self.grid[y][x] in '@P!'
         except IndexError: return False
     
-    def mines_adjacentes(self, x, y):
-        ww = self.verifier_case(x - 1, y)
-        nw = self.verifier_case(x - 1, y - 1)
-        nn = self.verifier_case(x, y - 1)
-        ne = self.verifier_case(x + 1, y - 1)
-        ee = self.verifier_case(x + 1, y)
-        se = self.verifier_case(x + 1, y + 1)
-        ss = self.verifier_case(x, y + 1)
-        sw = self.verifier_case(x - 1, y + 1)
+    def adjacent_mines(self, x, y):
+        ww = self.check_tile(x - 1, y)
+        nw = self.check_tile(x - 1, y - 1)
+        nn = self.check_tile(x, y - 1)
+        ne = self.check_tile(x + 1, y - 1)
+        ee = self.check_tile(x + 1, y)
+        se = self.check_tile(x + 1, y + 1)
+        ss = self.check_tile(x, y + 1)
+        sw = self.check_tile(x - 1, y + 1)
         return ww + nw + nn + ne + ee + se + ss + sw
     
-    def ouvrir_cases(self, x, y):
-        z = self.mines_adjacentes(x, y)
+    def open_tiles(self, x, y):
+        z = self.adjacent_mines(x, y)
         if z == 0:
             try:
                 self.grille[y][x] = ' '
@@ -79,34 +77,34 @@ class Grille(object):
                 self.open_tiles(x - 1, y + 1)
             except: pass
         else:
-            self.grille[y][x] = str(z)
+            self.grid[y][x] = str(z)
     
-    def cliquer(self, x, y):
+    def click(self, x, y):
         if self.grille[y][x] == '.':
-            self.ouvrir_cases(x, y)
-            self.victoire = not [l for l in self.grille if '@' in l]
-        elif self.grille[y][x] == '@':
-            self.grille[y][x] = '#'
-            self.fini = True
+            self.open_tiles(x, y)
+            self.vict = not [l for l in self.grid if '@' in l]
+        elif self.grid[y][x] == '@':
+            self.grid[y][x] = '#'
+            self.over = True
 
     
-    def marquer(self, x, y):
+    def flag(self, x, y):
         '''Switch between '.', 'F' and '?' if tile is safe, or '@', 'P', '!' if tile is mined.
 '''
         m = {'.': 'F', 'F': '?', '?': '.', '@': 'P', 'P': '!', '!': '@'}
-        self.grille[y][x] = m[self.grille[y][x]]
+        self.grid[y][x] = m[self.grid[y][x]]
 
 
-h, w, n = map(int, (get('Hauteur de la grille: '), get('Largeur de la grille: '), get('Nombre de mines: ')))
-grille = Grille(h, w, n)
+h, w, n = map(int, (get('Height of the grid: '), get('Width: '), get('Number of mines: ')))
+grid = Grid(h, w, n)
 
-while not grille.fini:
-    print(repr(grille))
+while not grid.fini:
+    print(repr(grid))
     a = ''
-    while a not in ('c', 'm'):
-        a = get('Action: [c]lic/[m]arque ')
+    while a not in ('c', 'f'):
+        a = get('Action: [c]lick/[f]lag ')
     x, y = map(int, (get('X = '), get('Y = ')))
     if a == 'c':
-        grille.cliquer(x, y)
-    elif a == 'm':
-        grille.marquer(x, y)
+        grid.click(x, y)
+    elif a == 'f':
+        grid.flag(x, y)
