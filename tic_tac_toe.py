af08be2812be9c988128cc0b12c65db46c08e91a
Moonstroke <Moonstroke@users.noreply.github.com>
2016-09-28 18:04:24 +0200

    Update and rename morpion.py to tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-def get(prompt):
+def get(prompt, allow_empty=False):
     try:
-       return input(prompt)
+        r = input(prompt)
     except EOFError:
         print('')
-        get(prompt)
+        return get(prompt)
     except KeyboardInterrupt:
         print('')
         exit()
+else: return r if r != '' or allow_empty else get(prompt)
     
 def _repr(g):
     r = '+-+-+-+\n'
@@ -19,35 +20,35 @@ def _repr(g):
     r += '+-+-+-+\n'
     return r
 
-def tour(g, x, y, k):
+def turn(g, x, y, k):
     if g[y][x] == ' ':
         g[y][x] = k
     else:
         raise Exception
 
-def fini(g):
+def over(g):
     _x = any([g[0][x] == g[1][x] == g[2][x] for x in range(3) if g[0][x] != ' '])
     _y = any([y[0] == y[1] == y[2] for y in g if y[0] != ' '])
     _diag = (g[0][0] == g[1][1] == g[2][2] or g[0][2] == g[1][1] == g[2][0]) and g[1][1] != ' '
     return _y or _x or _diag
 
-joueur = lambda ox: 'o' if ox else 'x'
+player = lambda ox: 'o' if ox else 'x'
 
-grille = [[' ' for _ in range(3)] for _ in range(3)]
+grid = [[' ' for _ in range(3)] for _ in range(3)]
 O_X = True #True <=> O, False <=> X
-victoire = False
-nb_tours = 1
-while not victoire and nb_tours < 10:
-    print(u'Tour n° : ' + str(nb_tours) + '\n' +  _repr(grille) + '\n\nC\'est au tour de : ' + joueur(O_X).upper())
+victory = False
+nb_turns = 0
+while not victory and nb_turns < 9:
+    print('Turn no : ' + str(nb_tours) + '\n' +  _repr(grille) + '\n\nPlaying : ' + joueur(O_X).upper())
     try:
-        tour(grille, int(get('x = ')), int(get('y = ')), joueur(O_X))
+        turn(grid, int(get('x = ')), int(get('y = ')), player(O_X))
     except Exception:
-        print('Non ! Recommence.')
+        print('Nope! Try again.')
         continue
     O_X = not O_X
-    nb_tours += 1
-    victoire = fini(grille)
-if victoire:
-    print(_repr(grille), '\n\nVictoire de ' + joueur(not O_X).upper() + ' !')
+    nb_turns += 1
+    victory = over(grid)
+if victory:
+    print(_repr(grid), '\n\nVictory of ' + player(not O_X).upper() + ' !')
 else:
-    print('Bloqués !')
+    print('Blocked!')
