ac765177cafca1cd0e80d66d1ce48b1cac32c8e1
Moonstroke <Moonstroke@users.noreply.github.com>
2016-10-03 17:56:26 +0200

    Add files via upload

---
+++ ipv42bin.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+def get(prompt='', allow_empty=False):
+    try:
+        r = input(prompt)
+    except EOFError:
+        print('')
+        get(prompt)
+    except KeyboardInterrupt:
+        print('')
+        exit()
+    else:
+        return r if r != '' or allow_empty else get(prompt)
+
+usage = '''USAGE: {} ADDR
+where ADDR is an IPv4 address (eg. 192.240.142.10)
+'''
+
+try:
+    exe, arg = sys.argv
+except ValueError:
+    print(usage, file = sys.stderr)
+    exit(1)
+
+for i in arg.split('.'):
+    print(bin(i)[2:])
