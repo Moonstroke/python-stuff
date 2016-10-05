f7f6ad4087471b633b736cdf28fdac937f8ec70a
Moonstroke <Moonstroke@users.noreply.github.com>
2016-10-05 18:09:10 +0200

    Update color_hex_neg.py

--- color_hex_neg.py
+++ color_hex_neg.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from sys import argv
+from sys import argv, stdin
 
-usage = '''USAGE: {} [#]HEXVAL
+usage = '''USAGE: {} [ [#]HEXVAL]
 Outputs the negative value of #HEXVAL
 HEXVAL may begin with an '#', however one will always be output.
+If called without argument, or argument is `-`, value is read from STDIN.
 '''.format(argv[0])
 
 try:
-    exe, arg = argv
+    exe, args = argv
 except:
-    print(usage)
-    exit(1)
-
-if arg[0] == '#':
-    arg = arg[1:]
-m = (1 << 4 * len(arg)) - 1
-print('#' + hex(m - int(arg, 16))[2:])
+    args = stdin.read()
+for arg in args.split('\n'):
+    if len(arg) == 0:
+        continue
+    try:
+        if arg[0] == '#':
+            arg = arg[1:]
+        m = (1 << 4 * len(arg)) - 1
+        print('#' + hex(m - int(arg, 16))[2:])
+    except:
+        print(usage)
+        exit(1)
